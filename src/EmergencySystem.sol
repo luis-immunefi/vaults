c998fedeecec8c4a44068647a6ef8bcd7b8a2fe5
Luis Alfredo Lorenzo <108485808+luis-immunefi@users.noreply.github.com>
2025-07-16 10:06:55 -0600

    Update EmergencySystem.sol

--- src/EmergencySystem.sol
+++ src/EmergencySystem.sol
@@ -18,7 +18,7 @@ contract EmergencySystem is Ownable2Step, IEmergencySystemEvents {
         _transferOwnership(_owner);
     }
 
-    function activateEmergencyShutdown() external onlyOwner {
+    function activateEmergencyShutdown() external {
         emergencyShutdownActive = true;
         emit EmergencyShutdownActivated(msg.sender);
     }
